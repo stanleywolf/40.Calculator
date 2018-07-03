5a90b90c5d62add0db452278e21438fd0e1e8443
stanleywolf <stanleyoffer@gmail.com>
2018-07-03 17:06:16 +0300

    test

---
+++ Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Mvc;
+using Calculator_CSharp.Models;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0m;
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "%":
+                    result = calculator.LeftOperand % calculator.RightOperand;
+                    break;
+                case "^":
+                    decimal powResult = 1;
+                    for (int i = 0; i < calculator.RightOperand; i++)
+                    {
+                        powResult *= calculator.LeftOperand;
+                    }
+                    result = powResult;
+                    break;
+            }
+            
+            return result;
+        }
+    }
+}
